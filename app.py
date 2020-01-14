3aa80ef7f7fad793cb39d96cd396a041e44ce178
irgiob <irgiob@gmail.com>
2020-01-14 19:04:29 +0700

    testing game start

--- app.py
+++ app.py
@@ -45,15 +45,10 @@ def callback():
         abort(400)
 
     return 'OK'
-
-NEW_TEXT = ''' Welcome to the Tic-Tac-Toe LINE BOT! 
-                To start, simply type \'start\' to start a game of 
-                Tic-Tac-Toe, then simply type the position
-                (e.g \'1 1\') to place a piece in the board.''' 
-FOLLOW_TEXT = 'You can also add me to groups, and play with your friends!'
+JOIN_TEXT = 'Welcome to the Tic-Tac-Toe LINE BOT!  To start, simply type \'start\' to start a game of Tic-Tac-Toe, then simply type the position (e.g  \'2 1\') to place a piece in the board.''
 LEAVE_TEXT = 'Thank you for playing, bye-bye!'
 games = []
-'''
+
 @handler.add(FollowEvent)
 def new_follower(event):
     line_bot_api.reply_message(
@@ -62,19 +57,16 @@ def new_follower(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=FOLLOW_TEXT))
-'''
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
-    '''txt = event.message.text
+    txt = event.message.text
     if txt == 'start':
         board = start_game()
         output = print_board(board)
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=output))'''
+            TextSendMessage(text=output))
     '''
     if txt == 'start':
         game_num = len(games)
