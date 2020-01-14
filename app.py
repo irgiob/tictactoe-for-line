30f59b6be8e9f145dca02f6b5422ab2687e4c36d
irgiob <irgiob@gmail.com>
2020-01-14 20:50:18 +0700

    added full game functionallity

--- app.py
+++ app.py
@@ -29,7 +29,6 @@ handler = WebhookHandler(
            default=os.environ.get('LINE_CHANNEL_SECRET'))
 )
 
-
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
@@ -45,35 +44,76 @@ def callback():
         abort(400)
 
     return 'OK'
+
 JOIN_TEXT = 'Welcome to the Tic-Tac-Toe LINE BOT!  To start, simply type \'start\' to start a game of Tic-Tac-Toe, then simply type the position (e.g  \'2 1\') to place a piece in the board.'
 LEAVE_TEXT = 'Thank you for playing, bye-bye!'
-games = []
-
-@handler.add(FollowEvent)
-def new_follower(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=NEW_TEXT))
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=FOLLOW_TEXT))
+games = {}
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
     txt = event.message.text
-    if txt == 'start':
-        board = start_game()
-        output = print_board(board)
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=output))
-    '''
-    if txt == 'start':
-        game_num = len(games)
-        if isinstance(event.source, SourceUser):
-            
-        elif isinstance(event.source, SourceRoom) or isinstance(event.source, SourceGroup):
-    '''
+    move = [int(num) for num in txt.split() if num.isdigit()]
+
+    if isinstance(event.source, SourceUser):
+        userID = event.source.user_id.text
+        if txt == 'start':
+            if not (userID in games):
+                games[userID] = start_game()
+                output = print_board(games[userID])
+                output += 'Choose where to put piece \'row col\''
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text=output))
+            else:
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text='Game already started.'))
+        elif (userID in games):
+            if txt == 'end':
+                games.pop(userID)
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text='Game Over. Thanks for playing!'))
+            elif len(move) == 2:
+                if is_invalid_move(games[userID],move) == 1:
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(text='Out of bounds, try again.'))
+                elif is_invalid_move(games[userID],move) == 2:
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(text='Space already filled, try again.'))
+                else:
+                    games[userID] = play_round(games[userID],1,move)
+                    if check_win(board,1) == True or is_full(board) == True:
+                        output = print_board(games[userID])
+                        games.pop(userID)
+                        if check_win(board,1) == True:
+                            output += 'You win!\nGame Over. Thanks for playing!'
+                        elif is_full(board) == True:
+                            output += 'It\'s a tie!\nGame Over. Thanks for playing!'
+                        line_bot_api.reply_message(
+                            event.reply_token,
+                            TextSendMessage(text=output))
+                    else:
+                        AI_move = AI_choose(games[userID])
+                        games[userID] = play_round(games[userID],-1,move)
+                        if check_win(board,-1) == True or is_full(board) == True:
+                            output = print_board(games[userID])
+                            games.pop(userID)
+                            if check_win(board,-1) == True:
+                                output += 'AI wins!\nGame Over. Thanks for playing!'
+                            elif is_full(board) == True:
+                                output += 'It\'s a tie!\nGame Over. Thanks for playing!'
+                            line_bot_api.reply_message(
+                                event.reply_token,
+                                TextSendMessage(text=output))
+            elif len(move) != 2:
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text='Invalid Move, type \'row col\' where you want to place your piece.'))            
+
+# for multiplayer (later)
 '''
 @handler.add(JoinEvent)
 def handle_join(event):
@@ -87,6 +127,7 @@ def leave_message(event):
         event.reply_token,
         TextSendMessage(text=LEAVE_TEXT))
 '''
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
--- game_func.py
+++ game_func.py
@@ -1,24 +1,67 @@
 # tictactoe-for-line/game_func.py
-
+from random import randint
 BOARD_SIZE = 3
 
+# initializes empty board
 def start_game():
     game_board = [[0,0,0],[0,0,0],[0,0,0]]
     return game_board
 
+# inserts move into board
+def play_round(board, player, move):
+    board[move[0]-1][move[1]-1] = player
+    return board
+
+# checks if board is full
+def is_full(board):
+    for i in range(BOARD_SIZE):
+        for j in range(BOARD_SIZE):
+            if board[i][j] == 0:
+                return False
+    return True
+
+# check if there is a winning condition for the player
+def check_win(board, player):
+    for i in range(BOARD_SIZE):
+        if (board[i][0]+board[i][1]+board[i][2] == 3*player or
+            board[0][i]+board[1][i]+board[2][i] == 3*player):
+            return True
+    if (board[0][0]+board[1][1]+board[2][2] == 3*player or 
+        board[0][2]+board[1][1]+board[2][0] == 3*player):
+        return True
+    return False
+
+# check if move made is invalid
+def is_invalid_move(board, move):
+    row = move[0]
+    col = move[1]
+    if (row < 1 or col < 1 or row > BOARD_SIZE or col > BOARD_SIZE):
+        return 1
+    elif board[row-1][col-1] != 0:
+        return 2
+    return 0
+
+# convert board data to board string for export
 def print_board(board):
     count = 1
-    output = '    1   2   3  \n  -------------\n'
+    output = '    1   2   3  \n  ---------\n'
     for i in range(BOARD_SIZE):
         output += f'{count} '
         count += 1
         for j in range(BOARD_SIZE):
             output += '| '
             if board[i][j] == 0:
-                output += '  '
+                output += '   '
             elif board[i][j] < 0:
-                output += 'X '
+                output += 'x '
             elif board[i][j] > 0:
-                output += 'O '
-        output += '|\n  -------------\n'
+                output += 'o '
+        output += '|\n  ---------\n'
     return output
+
+# AI chooses where to play randomly (will update with proper AI)
+def AI_choose(board):
+    move = [randint(1,3), randint(1,3)]
+    while is_invalid_move(board, move) == True:
+        move = [randint(1,3), randint(1,3)]
+    return move
